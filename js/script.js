c4de41e1efa21134562dd28cc1286755ac12fb5e
John Ault <john.ault.dev@gmail.com>
2023-07-17 21:03:15 -0400

    added avg

--- js/script.js
+++ js/script.js
@@ -8,13 +8,22 @@
 */
 
 const grades = [56, 87, 99, 40]; //Create an array called grades and put the following values:  56, 87, 99, 40
-  console.log(grades);
+console.log(grades);
 grades.push(68); // Add another element at the end of the array with value: 68                  56, 87, 99, 40, 68
-  console.log(grades);
+console.log(grades);
 grades.shift(); // Remove the 1st element of the array                                          87, 99, 40, 68
-  console.log(grades);
+console.log(grades);
 grades[2] = 60; // Change 40 to 60.                                                             87, 99, 60, 68
-  console.log(grades);
+console.log(grades);
+let average = 0
+average += grades[0];
+average += grades[1];
+average += grades[2];
+average += grades[3];
+average = average / grades.length;
+
+console.log(average);
+
 
 
 /*Q2:
@@ -29,18 +38,16 @@ Feel free to put any value.
 */
 
 const vehicle = {
-    make: "Ram",
-    brand: "2500",
-    year: 2019,
-    numWheels: "three",
-    isEnvironmentFriendly: false
-  };
+  make: "Ram",
+  brand: "2500",
+  year: 2019,
+  numWheels: "three",
+  isEnvironmentFriendly: false
+};
 
-  console.log(vehicle);
+console.log(vehicle);
 
-
-  // Loop through years
-  for (let x in vehicle) {
-    console.log(x + " " + vehicle[x]);
-  }
-  
+// Loop through years
+for (let x in vehicle) {
+  console.log(x + " " + vehicle[x]);
+}
---
+++ script.js
@@ -0,0 +1,20 @@
+const cars = [
+    "Saab",
+    "Volvo",
+    "BMW"
+  ];
+
+  console.log(cars.length);
+  console.log(cars.join("-"));
+  console.log(cars.indexOf("o"));
+
+  cars.concat(['a', 'b', 'c'])
+  console.log(cars);
+
+  cars.push("a");
+  console.log(cars);
+
+  cars.pop();
+  console.log(cars);
+
+
